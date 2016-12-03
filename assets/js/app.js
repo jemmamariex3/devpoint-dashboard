48bae4aaa4981fca382bbe1f1d5c4f652a7941cc
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-12-02 20:51:15 -0800

    added base controller to hold main variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,12 +5,20 @@ app.config(function ($interpolateProvider) {
   $interpolateProvider.startSymbol('[[');
   $interpolateProvider.endSymbol(']]');
 });
-app.controller('services', function ($scope, $http) {
+
+app.service('sharedData', function() {
+  $scope
+});
+
+app.controller('appController', function($scope){
   $scope.firstName = "John";
   $scope.lastName = "Doe";
+});
+
+app.controller('services', function ($scope, $http) {
 
 
-  $scope.updateServices = function(){
+  $scope.updateServices = function () {
     $scope.services.push('new service')
   };
 
@@ -18,10 +26,8 @@ app.controller('services', function ($scope, $http) {
     name: "Html"
   };
 
-
-
-  $scope.delete = function(id){
-    $http.get("https://devpoint-api.herokuapp.com/service/delete/" + id).then(function(response){
+  $scope.delete = function (id) {
+    $http.get("https://devpoint-api.herokuapp.com/service/delete/" + id).then(function (response) {
       $scope.reloadServices();
     })
   };
@@ -29,33 +35,24 @@ app.controller('services', function ($scope, $http) {
   $scope.reloadServices = function () {
     $http.get("https://devpoint-api.herokuapp.com/user/jarellano/services").then(function (response) {
       $scope.services = response.data;
-    }, function(response) {
+    }, function (response) {
       $scope.services = "Something went wrong";
     });
   };
   $scope.reloadServices();
 
-  $scope.submitForm = function() {
+  $scope.submitForm = function () {
     // Posting data to php file
     $http({
-      method  : 'POST',
-      url     : 'https://devpoint-api.herokuapp.com/user/jarellano/services',
-      data    : $.param($scope.user), //forms user object
-      headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+      method: 'POST',
+      url: 'https://devpoint-api.herokuapp.com/user/jarellano/services',
+      data: $.param($scope.user), //forms user object
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     })
-      .then(function(data) {
+      .then(function (data) {
         $scope.reloadServices();
       });
   };
 
 });
 
-
-
-
-// app.directive("serviceTemplate", function () {
-//   return {
-//     template: " <h1 ng-repeat='x in services'>[[x]]</h1>"
-//
-//   };
-// });
