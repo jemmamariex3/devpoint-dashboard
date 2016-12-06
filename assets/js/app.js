425586ce75ac5dd4a41350108ca507e0ff983d4a
Ramiro Lopez <Ramiro@Ramiros-MacBook-Pro.local>
2016-12-05 23:11:51 -0800

    Made changes to Delete/ Create for Services and Skills

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,39 +10,42 @@ app.service('sharedData', function() {
  // $scope
 });
 
-app.controller('appController', function($scope){
-  $scope.firstName = "John";
-  $scope.lastName = "Doe";
-  $scope.services = {};
-});
 
-app.controller('services', function ($scope, $http) {
-  $scope.delete = function (id) {
-    $http.get("https://devpoint-api.herokuapp.com/service/delete/" + id).then(function (response) {
-      $scope.reloadServices();
+app.controller('appController', function ($scope, $http) {
+
+  //GLOBAL VARIABLES
+  $scope.deleteItem = function (id, itemName) {
+    $http.delete("https://devpoint-api.herokuapp.com/user/jarellano/" + itemName + "/" + id).then(function (response) {
+      $scope.reloadData();
     })
   };
 
-  $scope.reloadServices = function () {
+
+  $scope.newItem = {};
+  $scope.newItemMethod = "Create";
+
+  $scope.reloadData = function () {
     $http.get("https://devpoint-api.herokuapp.com/user?username=jarellano").then(function (response) {
-      $scope.services = response.data;
+      $scope.user = response.data;
     }, function (response) {
-      $scope.services = "Something went wrong";
+      $scope.user = "Something went wrong";
     });
   };
-  $scope.reloadServices();
+  $scope.reloadData();
 
-  $scope.submitForm = function () {
+  $scope.createItem = function (itemName) {
     // Posting data to php file
     $http({
       method: 'POST',
-      url: 'https://devpoint-api.herokuapp.com/user/jarellano/services',
-      data: $.param($scope.user), //forms user object
+      url: 'https://devpoint-api.herokuapp.com/user/jarellano/' + itemName,
+      data: $.param($scope.newItem), //forms user object
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     })
       .then(function (data) {
-        $scope.reloadServices();
+        $scope.reloadData();
+        $scope.newItem = {};
       });
   };
+
 
 });
