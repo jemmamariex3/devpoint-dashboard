aabac2880a49fce6c73c27dc988777982a2c883a
Ramiro Lopez <Ramiro@Ramiros-MacBook-Pro.local>
2016-12-07 22:16:41 -0800

    Added add-project modal and Cleaned up code a bit, Add service was triggering add skillm modal, add skill modal works now.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -11,52 +11,42 @@ var User = db('api/data/user.json');
 
 
 module.exports = {
+    /**
+     * `UserController.create()`
+     */
+    create: function(req, res) {
+        var r = req.body;
 
-
-
-  /**
-   * `UserController.create()`
-   */
-  create: function (req, res) {
-    var r = req.body;
-
-    User.find({username: r.username}).then(function (data) {
-      if(data.length >= 1){
-        res.send("User already exists")
-      }
-      else{
-        bcrypt.genSalt(10, function(err, salt) {
-          bcrypt.hash(r.password, salt, function(err, hash) {
-            if (err) {
-              console.log(err);
-              cb(err);
+        User.find({ username: r.username }).then(function(data) {
+            if (data.length >= 1) {
+                res.send("User already exists")
             } else {
-              r.password = hash;
-              User.insert(
-                {
-                  "username": r.username,
-                  "password": r.password
-                })
-                .then(function (u) {
-                  res.send(u);
+                bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(r.password, salt, function(err, hash) {
+                        if (err) {
+                            console.log(err);
+                            cb(err);
+                        } else {
+                            r.password = hash;
+                            User.insert({
+                                    "username": r.username,
+                                    "password": r.password
+                                })
+                                .then(function(u) {
+                                    res.send(u);
+                                });
+                        }
+                    });
                 });
             }
-          });
+        })
+    },
+    /**
+     * `UserController.delete()`
+     */
+    delete: function(req, res) {
+        return res.json({
+            todo: 'delete() is not implemented yet!'
         });
-      }
-    })
-
-
-  },
-
-
-  /**
-   * `UserController.delete()`
-   */
-  delete: function (req, res) {
-    return res.json({
-      todo: 'delete() is not implemented yet!'
-    });
-  }
+    }
 };
-
