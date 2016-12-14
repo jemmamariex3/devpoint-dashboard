f3163645e81175f94de16989a867a121f06ca377
Jemma Marie <jemma.tiongson.420@my.csun.edu>
2016-12-13 23:41:01 -0800

    fixed code error for form validation

--- assets/style/js/formValidation.js
+++ assets/style/js/formValidation.js
@@ -1,10 +1,11 @@
-function textCounter(field,field2,maxlimit)
+function textCounter(field,counter,maxlimit)
 {
- var countfield = document.getElementById(field2);
- if ( field.value.length > maxlimit ) {
-  field.value = field.value.substring( 0, maxlimit );
-  return false;
- } else {
-  countfield.value = maxlimit - field.value.length;
- }
+ var countfield = document.getElementById(counter);
+if (field.value.length > maxlimit) {
+    field.value = field.value.substring(0, maxlimit);
+    return false;
+} else {
+    countfield.value = maxlimit - field.value.length;
+}
+
 }
